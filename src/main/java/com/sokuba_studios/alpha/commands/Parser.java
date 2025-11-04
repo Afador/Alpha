9724ed32779d3c9cd5e2593abf460f195650ca59
Afador <200121874+Afad0r@users.noreply.github.com>
2025-11-04 11:07:14 +0000

    - Combined CommandWords.java with Parser.java

--- src/main/java/com/sokuba_studios/alpha/commands/Parser.java
+++ src/main/java/com/sokuba_studios/alpha/commands/Parser.java
@@ -1,9 +1,21 @@
 package src.main.java.com.sokuba_studios.alpha.commands;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Parser {
     private static final Scanner READER = new Scanner(System.in);
+    private static final Map<String, String> validCommands = new HashMap<>();
+
+    static {
+        validCommands.put("go", "Move to another room");
+        validCommands.put("quit", "End the game");
+        validCommands.put("help", "Show help");
+        validCommands.put("look", "Look around");
+        validCommands.put("eat", "Eat something");
+        validCommands.put("teleport", "Teleport to another room");
+    }
 
     public static Command getCommand() {
         System.out.print("> ");
@@ -20,14 +32,22 @@ public class Parser {
             }
         }
 
-        if (CommandWords.isCommand(word1)) {
+        if (isCommand(word1)) {
             return new Command(word1, word2);
         } else {
             return new Command(null, word2);
         }
     }
 
+    public static boolean isCommand(String commandWord) {
+        return validCommands.containsKey(commandWord);
+    }
+
     public static void showCommands() {
-        CommandWords.showAll();
+        System.out.print("Valid commands are: ");
+        for (String command : validCommands.keySet()) {
+            System.out.print(command + " ");
+        }
+        System.out.println();
     }
 }
